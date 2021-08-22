11055c50d53b8436e1d105ccbe72ae2803986d27
flaribbit <1149761294@qq.com>
2021-08-22 22:20:21 +0800

    update pixiv api

--- main.py
+++ main.py
@@ -1,31 +1,28 @@
 import requests
 import json
-from bs4 import BeautifulSoup
 
 
-def getPage(n):
-    res = requests.get("https://www.pixiv.net/bookmark.php?rest=show&p="+str(n), headers={
+def getPage(n: int):
+    res = requests.get(f"https://www.pixiv.net/ajax/user/8046435/illusts/bookmarks?tag=&offset={n*100:d}&limit=100&rest=show&lang=zh", headers={
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36",
         "cookie": "PHPSESSID=62xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx3A",
     })
-    soup = BeautifulSoup(res.content)
-    items = soup.select(".image-item")
+    data = res.json()
     result = []
-    for item in items:
-        img = item.select_one("img")
-        result.append((item.select_one(".title").text,
-                       img["data-id"], img["data-tags"]))
+    for e in data["body"]["works"]:
+        result.append((
+            e["title"], e["id"], e["pageCount"],
+            e["userName"], e["userId"], e["tags"]))
     return result
 
 
-# %%
+# print(getPage(0))
 result = []
-for i in range(10):
+for i in range(20):
     try:
         result += getPage(i+1)
         print("[OK ]", i+1)
     except:
         print("[Err]", i+1)
-file = open("result.json", "w", encoding="utf8")
-file.write(json.dumps(result, ensure_ascii=False))
-file.close()
+with open("result.json", "w", encoding="utf8") as f:
+    json.dump(result, f, ensure_ascii=False)
